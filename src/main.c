a3216f801de95cd783b32a97daf61ed20c6106e5
steynvl <steyn.van.l@gmail.com>
2018-09-06 16:51:27 +0200

    Fixed bug when building path

--- src/main.c
+++ src/main.c
@@ -5,15 +5,15 @@
 #include <string.h>
 
 
-void list_dirs(char*, int);
-char* build_path(const char*, const char*);
-int is_file(const char*);
-
-char *base;
+void list_dirs(char *, int);
+char* build_path(const char *, const char *);
+int is_directory(const char *);
 
 
 int main(int argc, char *argv[])
 {
+    char *base;
+
     if (argc == 2 && (strcmp(argv[1], "--help") == 0)) {
         /* TODO */
         return 0;
@@ -34,8 +34,12 @@ void list_dirs(char *path, int level)
     dir = opendir(path);
 
     if (dir == NULL) {
-        fprintf(stderr, "Could not open directory '%s'\n", path);
-        exit(1);
+        if (level == 0) {
+            fprintf(stderr, "Could not open directory '%s'\n", path);
+            exit(1);
+        } else {
+            return;
+        }
     }
 
     while ((dir_entry = readdir(dir)) != NULL) {
@@ -44,8 +48,8 @@ void list_dirs(char *path, int level)
         }
 
         full_path = build_path(path, dir_entry->d_name);
-        printf("[%d] -- %s\n", is_file(full_path), full_path);
-        if (!is_file(full_path)) {
+        printf("[%d] -- %s\n", is_directory(full_path), full_path);
+        if (is_directory(full_path)) {
             list_dirs(full_path, level + 1);
         }
 
@@ -53,23 +57,26 @@ void list_dirs(char *path, int level)
     }
 
     closedir(dir);
-
 }
 
-int is_file(const char* path)
+int is_directory(const char *path)
 {
     struct stat ps;
     stat(path, &ps);
-    return S_ISREG(ps.st_mode);
+    return S_ISDIR(ps.st_mode);
 }
 
-char* build_path(const char* p1, const char* p2)
+char* build_path(const char *p1, const char *p2)
 {
     char *fp;
 
     fp = malloc(strlen(p1) + strlen(p2) + 2);
     strcpy(fp, p1);
-    strcat(fp, "/");
+
+    if (p1[strlen(p1) - 1] != '/') {
+        strcat(fp, "/");
+    }
+
     strcat(fp, p2);
 
     return fp;
